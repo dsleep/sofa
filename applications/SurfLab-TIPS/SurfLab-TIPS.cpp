bce9dc73335883dd1950dfaf7b04c42fc2ff0387
Saleh Dindar <saleh@cise.ufl.edu>
2016-03-10 13:44:51 -0500

    Proper error messages and and option to open file if the file name argument is wrong

--- applications/SurfLab-TIPS/SurfLab-TIPS.cpp
+++ applications/SurfLab-TIPS/SurfLab-TIPS.cpp
@@ -1,9 +1,34 @@
 #include "Visualizer.hpp"
 #include <QApplication>
+#include <QMessageBox>
+#include <QFileDialog>
+int openWindow(std::string argFile) {
+    // Loading the scene
+    std::string fileName = sofa::helper::system::DataRepository.getFile(argFile);
+    if(!QFile(fileName.c_str()).exists()) {
+        QMessageBox msg;
+        msg.setText("Cannot find the scene file");
+        msg.setInformativeText(QString("While looking for '%1'").arg(argFile.c_str()));
+        msg.exec();
+        return 1;
+    }
+    Node::SPtr root = sofa::simulation::getSimulation()->load(fileName.c_str());
+    sofa::simulation::getSimulation()->init(root.get());
+
+    Visualizer *w = new Visualizer(root);
+    w->setWindowFilePath(fileName.c_str());
+    w->setMessage(fileName.c_str());
+    w->show();
+    QApplication::setActiveWindow(w);
+    return  QApplication::exec();
+}
+
+const bool useFileDialog = true;
 
 int main(int argc, char** argv) {
   QApplication app(argc, argv);
   app.setApplicationName("SurfLab TIPS");
+
 
   // Initialize sofa
   sofa::core::ExecParams::defaultInstance()->setAspectID(0);
@@ -19,17 +44,29 @@ int main(int argc, char** argv) {
   sofa::helper::system::DataRepository.addFirstPath("/home/saleh/projects/sofa/share");
   sofa::simulation::setSimulation(new sofa::simulation::graph::DAGSimulation());
 
-  // Loading the scene
-  std::string fileName = sofa::helper::system::DataRepository.getFile(argv[1]);
-  Node::SPtr root = sofa::simulation::getSimulation()->load(fileName.c_str());
-  sofa::simulation::getSimulation()->init(root.get());
+  int ret;
+  if(argc < 2) {
+      if(useFileDialog){
+          QString fileName = QFileDialog::getOpenFileName(NULL, "Open SOFA scene", QString(), "Scene files (*.scn);;SaLua files (*.salua);;XML files (*.xml);; All files (*.*)");
+          if(!fileName.isEmpty())
+              openWindow(fileName.toStdString());
+          else
+              return 0;
+      } else {
+          QMessageBox msg;
+          msg.setText("Please provide a scene filename as the first argument.");
+          msg.setInformativeText(QString("Usage: %1 <filename>").arg(argv[0]));
+          msg.exec();
+          return 2;
+      }
+  } else
+    ret = openWindow(argv[1]);
 
+  sofa::simulation::graph::cleanup();
+  sofa::simulation::tree::cleanup();
+  sofa::simulation::common::cleanup();
 
-  Visualizer *w = new Visualizer(root);
-  w->setWindowFilePath(fileName.c_str());
-  w->setMessage(fileName.c_str());
-  w->show();
-  app.setActiveWindow(w);
-  return app.exec();
+  return ret;
+
 }
 
